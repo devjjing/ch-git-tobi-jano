6cbd7345254d2065873f350591301fdde64e4056
GATA5 <Tangjing.Gan@coop.ch>
2023-10-31 15:37:01 +0100

    Added Goodbye message

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         int tobi = 17;
         System.out.println("Hello World");
-        System.out.println("Alter:" + " " + tobi);
+        System.out.println("Alter Tobi:" + " " + tobi);
+        System.out.println("Goodbye");
     }
 }
