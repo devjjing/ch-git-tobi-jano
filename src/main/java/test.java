0a238323f18d953693fdffd5a2a55d4995c72294
GATA5 <Tangjing.Gan@coop.ch>
2023-10-31 15:12:49 +0100

    Added pom.xml Moved test.java to src

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,7 @@
 public class test {
     public static void main(String[] args) {
+        int tobi = 17;
         System.out.println("Hello World");
+        System.out.println("Alter:" + " " + tobi);
     }
-
-    int tobi = 17;
-
 }
